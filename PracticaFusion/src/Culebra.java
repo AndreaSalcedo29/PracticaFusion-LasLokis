f703f7a491545cb4ebec83340ac27dfdc70ec935
andreita <andreasalcee@iessampedro.com>
2022-04-06 19:43:09 +0200

    Creacion de culebra

--- PracticaFusion/src/Culebra.java
+++ PracticaFusion/src/Culebra.java
@@ -1,10 +1,18 @@
 
 public class Culebra extends Mascota {
 
+	String tipo; 
 	public Culebra(String nombre) {
 		super(nombre);
 		
 	}
+	public Culebra(String nombre, String tipo) {
+		super(nombre);
+		this.tipo = tipo;
+	}
+	
+	
+	
 	
 	
 
--- PracticaFusion/src/Principal.java
+++ PracticaFusion/src/Principal.java
@@ -2,7 +2,7 @@ public class Principal {
 	public static void main(String[] args) {
 		
 
-	Culebra miCulebra= new Culebra ("Juana"); 
+	Culebra miCulebra= new Culebra ("Juana", "Piton"); 
 	
 	miCulebra.MandarMensaje();
 
